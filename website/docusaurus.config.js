0d39e4962a3190725bc8278677cdc30eba66fd00
Yooking <wangyukun55555@gmail.com>
2022-01-18 21:51:06 +0800

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/YooKing',
               },
             ],
           },
