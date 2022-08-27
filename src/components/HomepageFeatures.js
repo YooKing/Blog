8e3c6aa2de03871ed308feed47bdb1dbf1108c44
Yooking <wangyukun55555@gmail.com>
2022-08-27 13:05:19 +0800

    Update HomepageFeatures.js

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -3,28 +3,7 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'YKMC',
-    description: (
-      <>
-        我创建了一个我的世界公益服YKMC，用纯净版或下载专用客户端开玩！
-        <a class="button button--primary" href="https://mc.yooking.top" target="_blank">YKMC</a>
-      </>
-    ),
-  },
-  {
-    title: '开服不易，为服务器捐款。',
-
-      description: (
-      <>
-
-      
-      <a class="button button--primary" href="https://afdian.net/@ykmc25565" target="_blank">去爱发电捐款</a>
-     
-      
-      </>
-    ),
-  },
+  
   {
     title: '路漫漫其修远兮，吾将上下而求索',
     description: (
