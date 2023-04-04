e17a7d8ba6759f7d061db6902dbfefdbaa2600c7
YooKing <wangyukun55555@gmail.com>
2023-04-04 23:26:05 +0800

    Update friend.ts

--- data/friend.ts
+++ data/friend.ts
@@ -1,10 +1,17 @@
 export const Friends: Friend[] = [
   {
     title: 'Yooking',
-    description: '...',
+    description: 'Me',
     website: 'https://yooking.top',
     avatar: '/img/friend/1.png',
   },
+  {
+    title: 'Jayden',
+    description: '为天地立心,为生民立命,为往圣继学,为万世开太平',
+    website: 'https://zengxud.top',
+    avatar: 'https://github.com/Jaaayden.png',
+  }
+
 ]
 
 export type Friend = {
